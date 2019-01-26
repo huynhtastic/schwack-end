3210736287379a1eefa787f8a7d546741f9c936a
Richard L Huynh <huynhtastic@gmail.com>
2019-01-25 21:01:01 -0600

    Barebone HTTP Server; added .gitignore

---
+++ services/database.js
@@ -0,0 +1,48 @@
+const crypto = require('crypto');
+const oracledb = require('oracledb');
+const dbConfig = require('../config/database.js');
+
+async function initialize() {
+  const pool = await oracledb.createPool(dbConfig.employeesPool);
+}
+
+async function close() {
+  await oracledb.getPool().close();
+}
+
+async function simpleExecute(statement, binds = [], opts = {}) {
+  return new Promise(async (resolve, reject) => {
+    let conn;
+
+    opts.outFormat = oracledb.OBJECT;
+    opts.autoCommit = true;
+
+    if (binds.password) {
+      binds.password = crypto.createHash('sha256').update(binds.password).digest('hex');
+    }
+
+    try {
+      conn = await oracledb.getConnection();
+      console.log('executing');
+      const result = await conn.execute(statement, binds, opts);
+      console.log('end');
+
+      resolve(result);
+    } catch (err) { 
+      reject(err);
+    } finally {
+      if (conn) { // conn worked, need to close
+        try {
+          await conn.close();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  });
+}
+
+module.exports.initalize = initialize;
+module.exports.close = close;
+module.exports.simpleExecute = simpleExecute;
+
---
+++ services/web-server.js
@@ -0,0 +1,91 @@
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const cors = require('cors');
+const webServerConfig = require('../config/web-server.js');
+const router = require('./router.js');
+const database = require('./database.js');
+
+let httpServer;
+
+function initialize() {
+  return new Promise((resolve, reject) => {
+    const app = express();
+    httpServer = http.createServer(app);
+
+    // Combines logging info from request and response
+    app.use(morgan('combined'));
+
+		// Parse incoming JSON requests and revive JSON
+		app.use(express.json({
+			reviver: reviveJson
+		}));
+
+    app.use(cors());
+
+    app.use('/api', router);
+
+    /* [BEGIN] user-session tracking */
+    // access cookies stored in browser
+    app.use(cookieParser());
+
+    // track logged-in user across sessions
+    app.use(session({
+      key: 'emp_id',
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        expires: 6000
+      }
+    }));
+
+    // check if user's cookie is saved in browser w/o user set: log them out
+    // happens when you stop express after login and cookie is still saved
+    app.use((req, res, next) => {
+      if (req.cookies.emp_id && !req.session.user) {
+        res.clearCookie('emp_id');
+      }
+      next();
+    });
+
+    var sessionChecker = (req, res, next) => {
+      if (req.session.user && req.cookies.emp_id) {
+        res.redirect('/dashboard');
+      } else {
+        next();
+      }
+    };
+
+    /* [END] user-session tracking */
+
+    httpServer.listen(webServerConfig.port)
+      .on('listening', () => {
+        console.log(`Web server listening on localhost:${webServerConfig.port}`);
+
+        resolve();
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+
+module.exports.initialize = initialize;
+
+function close() {
+  return new Promise((resolve, reject) => {
+    httpServer.close((err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports.close = close;
